72b13d7ee53088135566b60b1d5a474517d8f7c5
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-11-10 16:35:29 +0300

    Migration İşlemleri ve Abstract Interfaceler

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
--- CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -9,6 +9,7 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
 
 
     }
