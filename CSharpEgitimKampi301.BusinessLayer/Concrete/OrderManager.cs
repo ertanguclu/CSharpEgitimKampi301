00d2bdbb30f063cc440bf72f8d1acfc6fdefdb76
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-12-20 16:18:48 +0300

    Business Katmanı ve Logic Kurallar

---
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,43 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public void TDelete(Order entity)
+        {
+            _orderDal.Delete(entity);
+        }
+
+        public List<Order> TGetAll()
+        {
+            return _orderDal.GetAll();
+        }
+
+        public Order TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public void TDelete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
